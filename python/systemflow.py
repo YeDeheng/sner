310bdbfce3aa562d1af3cfb12028231cd3904e56
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-10-14 11:46:22 +0800

    sync

--- python/systemflow.py
+++ python/systemflow.py
@@ -7,13 +7,14 @@ import subprocess
 
 rawdatadir = './rawdata/'
 for file in os.listdir(rawdatadir):
-	filebase = '.'.join(file.split('.')[:-1]) if '.' in file else file
+	if file.endswith('.txt')
+		filebase = '.'.join(file.split('.')[:-1]) if '.' in file else file
 
-	tokenfile = rawdatadir+str(file)+".tk"
-	conllfile = rawdatadir+filebase+".conll"
+		tokenfile = rawdatadir+str(file)+".tk"
+		conllfile = rawdatadir+filebase+".conll"
 
-	mytokenizer.tokenize(rawdatadir+str(file), tokenfile)
-		
-	texttoconll.main(tokenfile, conllfile)
+		mytokenizer.tokenize(rawdatadir+str(file), tokenfile)
+			
+		texttoconll.main(tokenfile, conllfile)
 	
 	#subprocess.Popen("python conll02tostandoff.py -o ./rawdata/brat %s" % (conllfile), shell=True )
