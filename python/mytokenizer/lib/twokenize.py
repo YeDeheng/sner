fe3fddff7d60f8ddcf38c716db6ef212474b3a39
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-10-14 15:20:37 +0800

    improve tokenzer! I hate tokens!

--- python/mytokenizer/lib/twokenize.py
+++ python/mytokenizer/lib/twokenize.py
@@ -61,7 +61,7 @@ Email = EmailName + "@" + EmailDomain
 
 Email_RE = mycompile(Email)
 
-API = regex_or(r'((\w+)\.)+\w+\(\)', r'\w+\(\)', r'((\w+)\.)+(\w+)', r'\.\w+')
+API = regex_or(r'((\w+)[\.\-])+\w+\(\)', r'\w+\(\)', r'((\w+)[\.\-])+(\w+)', r'\.\w+')
 API_RE = mycompile(API)
 plural = r'\w+\(s\)'
 plural_RE = mycompile(plural)
@@ -104,7 +104,7 @@ ProtectThese = [
     Hashtag,
     AtMention,
     Timelike,
-    NumNum,
+    #NumNum,
     NumberWithCommas,
     ArbitraryAbbrev,
     Separators,
