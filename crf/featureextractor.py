c29d123169f2ce45ff18d94c77f71f07a921a1bb
Deheng Ye <ye0014ng@e.ntu.edu.sg>
2015-10-23 11:48:17 +0800

    sync

--- crf/featureextractor.py
+++ crf/featureextractor.py
@@ -8,54 +8,89 @@ import subprocess
 def GetOrthographicFeatures(word):
     features = ''
     features += word.lower() + '\t'
-    # if(len(word) >= 4):
-    #     features.append("prefix=%s" % word[0:1].lower())
-    #     features.append("prefix=%s" % word[0:2].lower())
-    #     features.append("prefix=%s" % word[0:3].lower())
-    #     features.append("suffix=%s" % word[len(word)-1:len(word)].lower())
-    #     features.append("suffix=%s" % word[len(word)-2:len(word)].lower())
-    #     features.append("suffix=%s" % word[len(word)-3:len(word)].lower())
+    # prefix and suffix features
+    if(len(word) >= 4):
+        features += word[0:1].lower() + '\t'
+        features += word[0:2].lower() + '\t'
+        features += word[0:3].lower() + '\t'
+        features += word[len(word)-1:len(word)].lower() + '\t'
+        features += word[len(word)-2:len(word)].lower() + '\t'
+        features += word[len(word)-3:len(word)].lower() + '\t'
+    else:
+        features += '-\t-\t-\t' + '-\t-\t-\t'
+    #Substring features (don't seem to help)
+    #for i in range(1,len(word)-2):
+    #    for j in range(i+1,len(word)-1):
+    #        features.append("substr=%s" % word[i:j])
 
-        #Substring features (don't seem to help)
-        #for i in range(1,len(word)-2):
-        #    for j in range(i+1,len(word)-1):
-        #        features.append("substr=%s" % word[i:j])
-
+    # Initial capitalization
     if re.search(r'^[A-Z]', word):
-        features += '1\t'   # capitalization
+        features += '1\t'   
     else:
         features += '0\t'
-    # if re.search(r'^[A-Z]', word) and goodCap:
-    #     features.append('INITCAP_AND_GOODCAP')
+
+    # All capital
     if re.match(r'^[A-Z]+$', word):
         features += '1\t'  #ALLCAP
     else:
         features += '0\t'
-    # if re.match(r'^[A-Z]+$', word) and goodCap:
-    #     features.append('ALLCAP_AND_GOODCAP')
-    # if re.match(r'.*[0-9].*', word):
-    #     features.append('HASDIGIT')
-    # if re.match(r'[0-9]', word):
-    #     features.append('SINGLEDIGIT')
-    # if re.match(r'[0-9][0-9]', word):
-    #     features.append('DOUBLEDIGIT')
-    # if re.match(r'.*-.*', word):
-    #     features.append('HASDASH')
-    # if re.match(r'[.,;:?!-+\'"]', word):
-    #     features.append('PUNCTUATION')
+
+    # has digit
+    if re.match(r'.*[0-9].*', word):
+        features += '1\t'
+    else:
+        features += '0\t'
+    
+    # has dot
+    if re.match(r'.*\..*', word):
+        features += '1\t'
+    else:
+        features += '0\t'
+
+    # has both digit and dot
+    if re.match(r'.*[0-9].*', word) and re.match(r'.*\..*', word):
+        features += '1\t'
+    else:
+        features += '0\t'
+    # has () at the end
+    if re.match(r'\(\)$', word):
+        features += '1\t'
+    else:
+        features += '0\t'
+
+    # contains underscore
+    if re.match(r'.*\_.*', word):
+        features += '1\t'
+    else:
+        features += '0\t'
+
+    # has Capital inside
+    if re.match(r'.*[\w\_]+[A-Z][\w\_]+.*', word):
+        features += '1\t'
+    else:
+        features += '0\t'
+    
+    # has dash
+    if re.match(r'.*-.*', word):
+        features += '1\t'
+    else:
+        features += '0\t'
+    
     return features
 
-if __name__=='__main__':
 
+
+if __name__=='__main__':
     # read in annotated conll file
     fin = open(sys.argv[1], 'r')
     fout = open(sys.argv[2], 'w')
     for line in fin:
         line = line.strip()
-        (word, label) = line.split('\t')
-        OrthographicFeatures = GetOrthographicFeatures(word)
+        if line:
+            (word, label) = line.split('\t')
+            OrthographicFeatures = GetOrthographicFeatures(word)
 
-        allfeatures = word + '\t' + OrthographicFeatures + label
-        fout.write(allfeatures + '\n')
+            allfeatures = word + '\t' + OrthographicFeatures + label
+            fout.write(allfeatures + '\n')
     fout.close()
     fin.close()
